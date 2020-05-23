c4928c98a8d075bd752bdbf71c2c7d4a726e6a10
pireats-abhi <pireats.abhi@gmail.com>
2020-05-23 11:30:03 +0600

    Initial

---
+++ SpringScurityBasic/src/main/java/org/ajgroup/security/SpringScurityOneApplication.java
@@ -0,0 +1,13 @@
+package org.ajgroup.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringScurityOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringScurityOneApplication.class, args);
+	}
+
+}
---
+++ SpringScurityBasic/src/test/java/org/ajgroup/security/SpringScurityOneApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ajgroup.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringScurityOneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
