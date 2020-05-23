c42136ed7279ec64e326e18bda58978dd3b6aec3
pireats-abhi <pireats.abhi@gmail.com>
2020-05-23 11:31:46 +0600

    with JDBC

---
+++ SpringScurityJDBC/src/main/java/org/ajgroup/security/SpringScurityJdbcApplication.java
@@ -0,0 +1,13 @@
+package org.ajgroup.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringScurityJdbcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringScurityJdbcApplication.class, args);
+	}
+
+}
---
+++ SpringScurityJDBC/src/test/java/org/ajgroup/security/SpringScurityJdbcApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ajgroup.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringScurityJdbcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
